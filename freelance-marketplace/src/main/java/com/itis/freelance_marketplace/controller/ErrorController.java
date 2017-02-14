0c7c178f9129b6e51e253ef04fc0b69bbf1da978
Alexander Leontjev <sleontjev@yandex.ru>
2017-02-14 13:39:25 +0300

    add 404 (page not found) error handler

---
+++ freelance-marketplace/src/main/java/com/itis/freelance_marketplace/controller/ErrorController.java
@@ -0,0 +1,16 @@
+package com.itis.freelance_marketplace.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by alt on 14.02.17.
+ */
+@Controller
+public class ErrorController {
+    @RequestMapping(value = "/error404", method = RequestMethod.GET)
+    public String error404() {
+        return "404";
+    }
+}
